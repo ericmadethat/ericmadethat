415a15ed75080e24e8539b6679df9c905783d41b
Eric Nguyen <hello@ericdesigns.ca>
2017-03-15 19:11:18 -0700

    wip - fix nav for different aspects ratios

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 	function changeHeader() {
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
+		header.classList.toggle("header-background", (scrollTop >= 50 || document.body.classList.contains("nav-open")));
 	}
 
 	var didScroll = false;
@@ -27,11 +27,11 @@
 		changeHeader();
 	});
 
-	$("a[href*=\\#]").on("click", function (event) {
-		event.preventDefault();
+	// $("a[href*=\\#]").on("click", function (event) {
+	// 	event.preventDefault();
 
-		$("html, body").animate({
-			scrollTop: $(this.hash).offset().top
-		}, 500);
-	});
+	// 	$("html, body").animate({
+	// 		scrollTop: $(this.hash).offset().top
+	// 	}, 500);
+	// });
 })();
