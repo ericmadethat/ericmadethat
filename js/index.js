16f208ca95683c7ac498cf70ede0fff1855ac350
Eric Nguyen <hello@ericdesigns.ca>
2017-03-20 00:54:09 -0700

    Add tilt to logo

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ console.log('test');
 
 // Reveal Effect
 window.sr = ScrollReveal({ reset: true });
-sr.reveal('.hero-logo', {distance: '30px', duration: 1000});
+sr.reveal('.hero-logo-wrap', {distance: '30px', duration: 1000});
 sr.reveal('.hero-text-container .subtext', {distance: '100px', delay: 500, duration: 1000});
 sr.reveal('#blog', { origin: 'right' });
 
@@ -12,5 +12,5 @@ heroLogo.tilt({
   maxTilt: 15,
   // perspective: 1000,
   glare: true,
-  maxGlare: 1
+  maxGlare: 0.9
 })
