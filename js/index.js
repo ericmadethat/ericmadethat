3cde85214cd8031c6c4288f8f48006a29067c8aa
Eric Nguyen <hello@ericdesigns.ca>
2017-03-20 02:18:35 -0700

    Fix loading rellax.js issue

--- js/index.js
+++ js/index.js
@@ -5,12 +5,11 @@ console.log('test');
 window.sr = ScrollReveal({ reset: true });
 sr.reveal('.hero-logo-wrap', {distance: '30px', duration: 1000});
 sr.reveal('.hero-text-container .subtext', {distance: '100px', delay: 500, duration: 1000});
-sr.reveal('#blog', { origin: 'right' });
+
 
 heroLogo = $('.hero-logo');
 heroLogo.tilt({
   maxTilt: 15,
-  // perspective: 1000,
   glare: true,
   maxGlare: 0.9
-})
+});
