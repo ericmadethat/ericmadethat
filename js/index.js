b4fa9fc2f5f7d8b3b32ed17b1ba9228e36f3bce9
Eric Nguyen <hello@ericdesigns.ca>
2017-03-20 00:30:15 -0700

    Add tilt.js, scrollreveal.js, remove velocity.js"

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log('index.js reached!!!');
+
+console.log('test');
+
+// Reveal Effect
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.hero-logo', {distance: '30px', duration: 1000});
+sr.reveal('.hero-text-container .subtext', {distance: '100px', delay: 500, duration: 1000});
+sr.reveal('#blog', { origin: 'right' });
+
+heroLogo = $('.hero-logo');
+heroLogo.tilt({
+  maxTilt: 15,
+  // perspective: 1000,
+  glare: true,
+  maxGlare: 1
+})
